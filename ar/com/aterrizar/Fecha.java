c295b864010abc6a061034cfc330e0551165c995
Gustavo C <p8queen@gmail.com>
2012-04-29 21:03:30 -0300

    Fecha + flexible, sin finalizar

--- ar/com/aterrizar/Fecha.java
+++ ar/com/aterrizar/Fecha.java
@@ -3,11 +3,18 @@ package ar.com.aterrizar;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * 
+ * @author Rodrigo De Luca
+ *
+ */
 public class Fecha {
 	
 	Date fecha;
-	String formatoDeStr;
+	String formatoDeStringDeEntrada;
 	SimpleDateFormat formatoDelTexto;
 		
 	public Date getFecha() {
@@ -16,23 +23,41 @@ public class Fecha {
 	
 
 	public void setFechaISO8601(String strFecha){
-		formatoDeStr = "yyyy-MM-dd";
+		formatoDeStringDeEntrada = "yyyy-MM-dd";
 		strToDate(strFecha);
 	}
 	
 	public void setFechaLatinoAmericana(String strFecha){
-		formatoDeStr = "dd/MM/yyyy";
+		formatoDeStringDeEntrada = "dd/MM/yyyy";
 		strToDate(strFecha);
 	}
 
 	public void setFechaNorteAmericana(String strFecha){
-		formatoDeStr = "yyyy-MM-dd";
+		formatoDeStringDeEntrada = "yyyy-MM-dd";
 		strToDate(strFecha);
 	}
+	/*
+	 * 
+	 * @param string flexible dd/mm/yyyy, d/mm/yyyy, dd/m/yyyy, d-m-yyyy, etc
+	 */
 	
+	/** @autor Rodrigo De Luca
+	 * @param flexible dd/mm/yyyy, 
+	 * d/mm/yyyy, dd/m/yyyy, d-m-yyyy, etc
+	 */
+	public void setFechaFlexible(String strFecha){
+		String input = "2-31-2012"; // mm-dd-yyyy/m-d-yyyy/mm/dd/yyyy
+		String expression1 = "^([01][012]|[1-9])[-/](0[1-9]|[12][0-9]|3[01]|[1-9])[-/](18|19|20|21)\\d{2}$";
+		Pattern p = Pattern.compile(expression1);
+		Matcher m = p.matcher(input);
+		if (m.find())
+			System.out.println("matched i1");
+		else
+			System.out.println("Error matching");		  
+	}
 	
 	private void strToDate(String strFecha){
-		formatoDelTexto = new SimpleDateFormat(formatoDeStr);
+		formatoDelTexto = new SimpleDateFormat(formatoDeStringDeEntrada);
 		try {
 			fecha = formatoDelTexto.parse(strFecha);
 		} catch (ParseException e) {
@@ -61,9 +86,7 @@ public class Fecha {
 	}
 	
 	
-	public void setFechaFlexible(String strFecha){
-		
-	}
+	
 
 }
 	
--- ar/com/aterrizar/FechaFlexible.java
+++
@@ -1,6 +0,0 @@
-package ar.com.aterrizar;
-
-public class FechaFlexible {
-	Fecha unaFecha;
-	
-}
